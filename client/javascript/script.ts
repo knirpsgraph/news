93f7a982f08a39db5872e53067e1a094fb748c1d
Hany Nicklas <60263068+knirpsgraph@users.noreply.github.com>
2021-02-08 22:54:45 +0100

    Alert dark optic

--- client/javascript/script.ts
+++ client/javascript/script.ts
@@ -63,7 +63,7 @@ function renderMessage(message: string) {
 
     // Create new alert
     const newAlert: JQuery = $(`
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div class="alert alert-dark fade show" role="alert">
             ${message}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
